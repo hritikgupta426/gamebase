7373f7f853e375920661a2945efd11dfd92b21d1
hritikgupta426 <79108995+hritikgupta426@users.noreply.github.com>
2021-03-08 23:01:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -34,6 +34,30 @@ else
 {
   window.alert("Please fill all the required details");
 }
+      
+       $("#btn-signup").click(function()
+    {
+      var email=$("#email").val();
+      var password=$("#password").val();
+      if(email!="" && password!="")
+      {
+        var result=firebase.auth().signInWithEmailAndPassword(email,password);
+
+        result.catch(function(error)
+{
+  var errorCode= error.code;
+  var errorMessage= error.message;
+
+   console.log(errorCode);
+   console.log(errorMessage);
+
+   window.alert("Message:" errorMessage);
+});
+}
+else
+{
+  window.alert("Please fill all the required details");
+}
 
     }
   );
