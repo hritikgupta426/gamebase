a17ee74e08befe40c622346eb71802e60ed0111c
hritikgupta426 <79108995+hritikgupta426@users.noreply.github.com>
2021-04-08 04:33:43 +0530

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -1,50 +1,41 @@
 var user=firebase.auth().currentUser;
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    document.getElementById("user_div").style.display="initial";
-    document.getElementById("login_div").style.display="none";
 
-  } else {
-    // No user is signed in.
-    document.getElementById("user_div").style.display="none";
-    document.getElementById("login_div").style.display="initial";
-
-  }
-});
 var button=document.querySelector("#btn-login");
 button.addEventListener('click',signIn);
 
 function signIn(){
 var userEmail=document.getElementById("email");
 var userPass=document.getElementById("password");
-firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-.then(()=>{
-return firebase.auth().signInWithEmailAndPassword(userEmail.value,userPass.value).then(cred=>{
-  window.location="C:/Users/Lenovo/Desktop/gamebase/allgames.html"
-})
-
-
-})
-
+firebase.auth().signInWithEmailAndPassword(userEmail.value, userPass.value)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+    window.location="C:/Users/Lenovo/Desktop/gamebase/gamebase.html";
+  })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-
-    window.alert("Error:"+errorMessage);
+    alert("Error Message"+ errorMessage)
   });
 };
 
 var button1=document.querySelector("#btn-signup");
-button1.addEventListener('click',signIn);
+button1.addEventListener('click',signUp);
 
 function signUp(){
-  var email1=document.getElementById("e-mail");
+  var email1=document.getElementById("email1");
   var pass=document.getElementById("pass");
-  firebase.auth().createUserWithEmailAndPassword(email1.value,pass.value).then(
-    cred=>{
-      alert("user registration successfull");
-    }
-  )
-}
-})();
+  firebase.auth().createUserWithEmailAndPassword(email1.value, pass.value)
+  .then(((userCredential) => {
+    // Signed in
+      var user = userCredential.user;
+alert("user registration successfull")
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+};
